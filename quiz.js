39aac0caa5cda86fc730651759bd530f55b7fdbe
TabithaAluel <alueltabitha@gmail.com>
2021-05-30 22:10:54 +0000

    quiz

---
+++ quiz.js
@@ -0,0 +1,33 @@
+function result(){
+var score=0;
+if (document.getElementById('correctAnswer1').checked)
+{
+  score ++
+
+}
+if (document.getElementById('correctAnswer1').checked)
+{
+  score ++
+}
+if (document.getElementById('correctAnswer1').checked)
+{
+  score ++
+
+}
+if (document.getElementById('correctAnswer1').checked)
+{
+  score ++
+
+}
+if (document.getElementById('correctAnswer1').checked)
+{
+  score ++
+
+}
+if (document.getElementById('correctAnswer1').checked)
+{
+  score ++
+}
+document.write("Your score is:"+score)
+
+}
